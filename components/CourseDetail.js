36c57d26adcbd573197700bda3c65b5d49f5279d
MirtizDev <134943798+MirtizDev@users.noreply.github.com>
2024-03-19 22:51:02 +0400

    fixing image error

--- components/CourseDetail.js
+++ components/CourseDetail.js
@@ -9,7 +9,7 @@ import OtherDesignCourses from './OtherDesingCourses';
 import OtherOfficeCourses from './OtherOfficeCourses';
 import classes from './CourseDetail.module.css'
 import NavDark from './NavDark';
-
+import Image from 'next/image';
 function CourseDetail({data}) {
   console.log(data)
   return (
@@ -19,7 +19,7 @@ function CourseDetail({data}) {
           <Nav/>
           <div className="container">
             <div className={classes["course-detail"]}>
-              <img src={`/images/${data[0].img}`} className={classes["course-detail__img"]}/>
+            <Image src={`/images/${data[0].img}`} className={classes["course-detail__img"]} width={270} height={270}/>
               <div className={classes["course-detail__content"]}>
                 <h2 className={classes["course-detail__name"]}>{data[0].name}</h2>
                 <p className={classes["course-detail__description"]}>{data[0].description}</p>
