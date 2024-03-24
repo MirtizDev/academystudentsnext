00b145ea05ec9b599dc261a64495a48ec4e4a30b
MirtizDev <134943798+MirtizDev@users.noreply.github.com>
2024-03-24 20:01:56 +0400

    fixing smthings

--- components/NavResponsive.js
+++ components/NavResponsive.js
@@ -6,7 +6,7 @@ import classes from './Nav.module.css'
 function NavResponsive() {
   return (
     <Link className={classes["nav__logo-wrap"]} href='/' style={{textDecoration:'none'}} id='start1'>
-        <Image src='/images/aclogo.png' alt='academy students logo' className={classes.nav__logo} width={80} height={49}/>
+        <Image src='/images/aclogo.png' alt='academy students logo' className={classes.nav__logo} width={60} height={49}/>
         {/* <img src='/images/aclogo.png' alt="" className={classes.nav__logo} /> */}
         <p style={{position:'relative'}}> <span>|</span>Academy Students <span className={classes.navdark__beta} style={{fontSize:'1.2rem'}}>beta</span></p>
     </Link>
