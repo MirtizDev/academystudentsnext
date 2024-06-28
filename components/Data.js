21301ff02669c920e666c683bea3c18580e40f1b
MirtizDev <134943798+MirtizDev@users.noreply.github.com>
2024-06-29 00:33:57 +0400

    changing datas 2

--- components/Data.js
+++ components/Data.js
@@ -59,48 +59,48 @@ export const AllCourses = [
         name:"Adobe Photoshop",
         img:'photoshop.png',
         description:"Qrafik dizayn dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Kohnə şəkillərin bərpası,Ağ-qara şəkillərin rənglənməsi,Üz montajı,Kollaj və panarama şəkillərin yiğılması,Buklet, flayer, banner, vizika,Poster,Retuş,Animasiya,Effektlər və.s",
-        price:90
+        price:100
     },
     {
         id:2,
         name:"Adobe Illustrator",
         img:'illustrator.png',
         description:"Qrafik dizayn dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Flat logo,Damla effekti,Poster,Vizitka, flayer, Buklet,Vectorların çəkilməsi,Cədvəl,diaqram,Effektlər",
-        price:90
+        price:100
     },
     {   id:3,
         name:"Corel Draw",
         img:'coreldraw.png',
         description:"Qrafik dizayn dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Logo,Buklet, flayer, vizitka,Təqvim,CMYK rəng ayrımı,Effektlər,Plokat,Poster,Pazl,Rastr şəklin vectora kecirilməsi",
-        price:90
+        price:100
     },
     {
         id:4,
         name:"Adobe InDesign",
         img:'indesign.png',
         description:"Qrafik dizayn dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Kohnə şəkillərin bərpası,Ağ-qara şəkillərin rənglənməsi,Üz montajı,Kollaj və panarama şəkillərin yiğılması,Buklet, flayer, banner, vizika,Poster,Retuş,Animasiya,Effektlər və.s",
-        price:120
+        price:100
     },
     {
         id:5,
         name:"Microsoft Word",
         img:'word.png',
         description:"Microsoft Ofis dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Metnlerin tertibi,Cedvel,Fayla wifre qoymaq,Senedlerin chapi,Sehife nomrelenmesi,Sehife bawliqi ve sehife sonluqu ve.s",
-        price:90
+        price:70
     },
     {
         id:6,
         name:"Microsoft Excel",
         img:'excel.png',
         description:"Microsoft Ofis dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Ticaret,Endirim,Emek haqqinin hesablanmasi,Kredit,Umumi cem,Konselidasiya(Cedvellerin arazinda elaqe qurmaq),Melumatin yoxlanmasi,Diaqram,Bir neche funksiya:ДВССЛ,ВПР,ЕСЛИ,ПОИСКПОЗ,ИНДЕКС ve.s.Valyutani deyiwmek Cemleme(Ededi orta, cemleme ve.s.),Bawliqi dondurmaq.Sehifye wifre qoymaq ve.s",
-        price:90
+        price:70
     },
     {
         id:7,
         name:"Microsoft PowerPoint",
         img:'powerpoint.png',
         description:"Microsoft Ofis dərslərini təqdim edirik.Dərslər həftə ərzində 3dəfə 60dəq və ya 2dəfə 90dəq həcmində təşkil olunur Məşğələlər fərdi şəkildə təşkil olunur.Proqram:Teqdimatlar,Slide show,Naviqator,Intro,Poster,Kichik hecmde video montajlar,Cedvel,Dioqram ve.s",
-        price:90
+        price:70
     },
 ]
 
